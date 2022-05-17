81c87fd23a24f3b669f685297befb4ce63105451
Di Wu <dwu233@gmail.com>
2022-05-17 19:47:29 -0400

    finally resolved the npm run bugs, thank you Rob...

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -9,3 +9,22 @@ module.exports = async ({ config, mode }) => {
     }
   };
 };
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       // ...Other rules,
+//       {
+//         test: /\.s[ac]ss$/i,
+//         use: [
+//           // Creates `style` nodes from JS strings
+//           'style-loader',
+//           // Translates CSS into CommonJS
+//           'css-loader',
+//           // Compiles Sass to CSS
+//           'sass-loader',
+//         ],
+//       },
+//     ],
+//   },
+// };
