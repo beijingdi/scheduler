55622a0894295e00bb90dd4cc4d8e859848b18ac
Di Wu <dwu233@gmail.com>
2022-05-20 01:51:57 -0400

    edit appointment form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import InterviewerList from "../InterviewerList.js";
+import Button from "../Button.js";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = (e) => {
+    setStudent('');
+    setInterviewer(null);
+    e();
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={(id) => {setInterviewer(id)}}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={()=>{reset(props.onCancel)}}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+};
