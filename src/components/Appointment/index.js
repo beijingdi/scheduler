5a3c8df3737bfdf54679d191aa010db285621775
Di Wu <dwu233@gmail.com>
2022-05-24 00:42:40 -0400

    error handling

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -18,6 +18,8 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -30,15 +32,22 @@ export default function Appointment(props) {
     };
     transition(SAVING);
     props.bookInterview(props.id,interview)
-    .then(() =>transition(SHOW));
+    .then(() =>transition(SHOW))
+    .catch((err) => {
+      console.log(err);
+      transition(ERROR_SAVE, true);
+    });
     
   };
 
   const cancel = (interviewId) => {
-    transition(DELETING);
+    transition(DELETING, true);
     props.cancelInterview(interviewId)
-    .then(() => transition(EMPTY));
-  }
+    .then(() => transition(EMPTY))
+    .catch((error) =>{
+      transition(ERROR_DELETE, true)
+    });
+  };
 
   return(
     <article className="appointment">
@@ -72,6 +81,8 @@ export default function Appointment(props) {
         onCancel={() => back()}
         onSave={save}
       />}
+      {mode === ERROR_SAVE && <Error message={"Cannot save"} onClose={() => back()}/>}
+      {mode === ERROR_DELETE && <Error message={"Cannot delete"} onClose={() => back()}/>}
     </article>
 
   );
